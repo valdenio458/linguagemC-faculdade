f751a13d204e726dc4e60b15dcd5b588c295be1d
valdenio458 <valdeniomartinho@gmail.com>
2023-08-22 19:41:50 -0300

    primeiros exercícios

---
+++ aula-1/ex-1/main.cpp
@@ -0,0 +1,24 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    char nome;
+    char endereco;
+    int idade;
+    
+    printf("Nome \n");
+    scanf("%s",&nome);
+    
+    printf("Endereco \n");
+    scanf("%s", &endereco);
+    
+    printf("Idade");
+    scanf("%d", &idade);
+    
+    printf("\n Nome: %s", nome);
+    printf("\n Endereco %d \n", idade);
+    
+    return 0;
+    
+}
