55da79f55a527e12eb1588d02bc350579be4723f
valdenio458 <valdeniomartinho@gmail.com>
2023-08-22 19:39:09 -0300

    inicia projeto

---
+++ aula-1/ex-2/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    float nota1;
+    float nota2;
+    float media;
+    
+    printf("Insira a primeira nota: \n");
+    scanf("%f",&nota1);
+    
+    printf("Insira a segunda nota \n");
+    scanf("%f", &nota2);
+    
+    media = (nota1 + nota2) / 2;
+    printf("A média das notas é: %f", media);
+
+    
+    return 0;
+    
+}
